3118d84adf68c9db92d90e27a483fc8f74b950ae
Fariz Hermawan <fariz.hermawan@traveloka.com>
2020-01-29 21:24:53 +0700

    Enable auto switch between local and production

--- source/classes/constant.ts
+++ source/classes/constant.ts
@@ -1,4 +1,6 @@
 export default class Constant {
 
-  static SERVICE_BASE_URL = "https://portal.riska.or.id/service/api";
+  static SERVICE_BASE_URL = window.location.hostname === 'portal.riska.or.id'
+    ? "https://portal.riska.or.id/service/api"
+    : "http://localhost:8000/api";
 }
