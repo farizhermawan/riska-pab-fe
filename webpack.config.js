4581df7686dba03954b82c9e18a662e247d8027c
Fariz Hermawan <fariz.hermawan@traveloka.com>
2020-02-22 10:38:27 +0700

    use another deploy strategy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CreateSymlinkPlugin = require('create-symlink-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const path = require('path');
 
@@ -19,14 +19,10 @@ module.exports = (env, argv) => {
     new HtmlWebPackPlugin({
       template: sourcePath + '/index.html'
     }),
-    new CreateSymlinkPlugin([
-      {origin: '../views', symlink: 'views'},
-      // {origin: '../static/images', symlink: 'images'}, TODO: Research why this is failing
-      {origin: '../static/css', symlink: 'css'},
-      {origin: '../static/js', symlink: 'js'},
-      {origin: '../static/login.html', symlink: 'login.html'},
-      {origin: '../static/.htaccess', symlink: '.htaccess'},
-    ]),
+    new CopyWebpackPlugin([
+      {from: 'static'},
+      {from: 'views', to: distPath + '/views'},
+    ])
   ];
 
   plugins.push(new webpack.NamedModulesPlugin(), new webpack.HotModuleReplacementPlugin());
