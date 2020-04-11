2e0943a4497f47cef2c890f88a0cfecc504c61aa
Fariz Hermawan <farizhermawan.id@gmail.com>
2020-04-12 02:17:47 +0700

    Change env

--- source/app/constants/environment.ts
+++ source/app/constants/environment.ts
@@ -2,5 +2,5 @@ export default class Environment {
 
   static SERVICE_BASE_URL = window.location.hostname === 'localhost'
     ? "http://localhost:8000"
-    : "https://portal.riska.or.id/api";
+    : "https://api.riska.or.id";
 }
